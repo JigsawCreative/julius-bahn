922280f62559d3cb10a1d260ed7ed5899762f6a3
Neil <johnnycocheroo454@gmail.com>
2020-10-21 17:38:10 +0100

    Revised bottom right design

--- brochure-modal.php
+++ brochure-modal.php
@@ -3,15 +3,54 @@
 	const $ = jQuery;
 
 	$(window).scroll(function(){        
-        if($(document).scrollTop() > 2000 && $('#brochureModal').attr('displayed') === 'false') {
-            $('#brochureModal').modal('show');
+        if($(document).scrollTop() > 100 && $('#brochureModal').attr('displayed') === 'false') {
+            console.log('scroll');
+            $('#brochureModal').fadeIn(500);
             $('#brochureModal').attr('displayed', 'true');
+
+            $('.brochure-modal-close').click(function() {
+                $('#brochureModal').fadeOut(500);
+            });
+
+            $('.close-text-btn').click(function() {
+                $('#brochureModal').fadeOut(500);
+            });
         }
      }); 
 	
-	</script>
+    </script>
+    
+    <div class="modal" id="brochureModal" tabindex="-1" role="dialog" aria-labelledby="brochureModal" aria-hidden="true" displayed="false">
+	<div class="modal-dialog bottom-right-fixed" role="document">
+		<div class="modal-content">
+		<div class="modal-header">
+			<h5 class="modal-title" id="exampleModalLongTitle">Get Our New 2020 Brochure</h5>
+			<button type="button" class="brochure-modal-close" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+			</button>
+		</div>
+		<div class="modal-body brochure-details">
+            <div class="brochure-20-image">
 
-	<div class="modal fade" id="brochureModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true" displayed="false">
+                <a href="/request-a-brochure/"><img src="/wp-content/uploads/JuliusBahn_Brochure-2020_cover.jpg" alt="Julius Bahn 2020 Brochure"></a>
+
+            </div>
+			
+            <div class="brochure-20-text">
+            
+                <p>Request a copy of our 2020 brochure to view all of our latest designs, handcrafted in South Staffordshire.</p>
+
+            </div>
+		</div>
+		<div class="modal-footer">
+			<button type="button" class="btn close-text-btn">Close</button>
+			<a href="/request-a-brochure/" class="btn">Get Brochure</a>
+		</div>
+		</div>
+	</div>
+	</div>
+
+	<!-- <div class="modal fade" id="brochureModal" tabindex="-1" role="dialog" aria-labelledby="brochureModal" aria-hidden="true" displayed="false">
 	<div class="modal-dialog modal-dialog-centered" role="document">
 		<div class="modal-content">
 		<div class="modal-header">
@@ -39,4 +78,4 @@
 		</div>
 		</div>
 	</div>
-	</div>
+	</div> -->

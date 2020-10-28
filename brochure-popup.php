bf68b448ca3756e17a978a2c0c2c995fc5476dea
Neil <johnnycocheroo454@gmail.com>
2020-10-28 16:34:55 +0000

    Brochure popup template change and logic

--- brochure-popup.php
+++ brochure-popup.php
@@ -8,29 +8,6 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
-
-?>
-
-<script>
-
-	const $ = jQuery;
-
-	$(window).scroll(function(){        
-        if($(document).scrollTop() > 10 && $('.brochure-popup').attr('closed') === 'false') {
-            console.log('scroll');
-            $('.brochure-popup').fadeIn(500);
-        }
-     }); 
-
-     $(document).ready(function() {
-         $('.brochure-popup__close').click(function(){
-            $('.brochure-popup').fadeOut(500).attr('closed', 'true');
-         });
-     });
-	
-</script>
-
-<?php 
     $fullUrl = $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; 
   
   $host = basename($fullUrl);
